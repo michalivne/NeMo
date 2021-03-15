b3bccc71b06907901c3d15036cf7e7e6b9f92746
Micha Livne <mlivne@nvidia.com>
2021-03-15 12:46:12 +0200

    1. Debugging.

--- nemo/collections/nlp/models/language_modeling/transformer_lm_model.py
+++ nemo/collections/nlp/models/language_modeling/transformer_lm_model.py
@@ -41,6 +41,7 @@ class TransformerLMModel(ModelPT):
     def __init__(self, cfg: DictConfig, trainer: Trainer = None):
 
         # Get global rank and total number of GPU workers for IterableDataset partitioning, if applicable
+        import pudb; pudb.set_trace()
         self.global_rank = 0
         self.world_size = 1
         if trainer is not None:
