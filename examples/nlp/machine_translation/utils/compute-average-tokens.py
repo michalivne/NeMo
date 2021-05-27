59b906f3aac2817a38a0bb1856ee7b1ce13a85d1
Micha Livne <mlivne@nvidia.com>
2021-05-27 13:43:26 -0400

    1. Debugging.

--- examples/nlp/machine_translation/utils/compute-average-tokens.py
+++ examples/nlp/machine_translation/utils/compute-average-tokens.py
@@ -6,8 +6,10 @@ Prints the evrage token number from a tarred dataset.
 import pickle
 import glob
 import numpy as np
+import sys
 
-files = glob.glob("*.pkl")
+# files = glob.glob("*.pkl")
+files = sys.argv[1:]
 all_len = []
 
 for fn in files:
