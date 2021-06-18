28071d4fd407a7cb6c0fb3110655d54c58eeff04
Micha Livne <mlivne@nvidia.com>
2021-06-18 09:45:51 -0400

    1. Debugging.

--- nemo/collections/nlp/models/machine_translation/mt_enc_dec_model.py
+++ nemo/collections/nlp/models/machine_translation/mt_enc_dec_model.py
@@ -1377,6 +1377,7 @@ class MTMIMModel(MTEncDecModel):
         """
         mode = self.training
         try:
+            import pudb; pudb.set_trace()
             self.eval()
             src_hiddens = self.encoder(input_ids=src, encoder_mask=src_mask)
 
