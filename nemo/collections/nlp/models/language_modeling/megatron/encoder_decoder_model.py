189b8b79bcf150b47ed4d17e1b07b4e660f1c9af
Micha Livne <mlivne@nvidia.com>
2021-11-23 16:22:12 +0200

    1. Debugging.
    Signed-off-by: Micha Livne <mlivne@nvidia.com>

--- nemo/collections/nlp/models/language_modeling/megatron/encoder_decoder_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron/encoder_decoder_model.py
@@ -110,8 +110,6 @@ class EncoderDecoderModel(MegatronModule):
         self.post_process = post_process
         self.fp16_lm_cross_entropy = fp16_lm_cross_entropy
 
-        assert not (fused_fp16 and fused_bf16), "both fused_fp16 and fused_bf16 flags cannot be True at the same time."
-
         if kv_channels is None:
             assert (
                 hidden_size % num_attention_heads == 0
